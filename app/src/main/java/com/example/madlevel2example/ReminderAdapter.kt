39c200274cc9ab7e517266e9e053b1e51d73b457
Rick Schut <rick-schut@live.nl>
2020-09-09 08:57:56 +0200

    Finished tutorial

---
+++ app/src/main/java/com/example/madlevel2example/ReminderAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.madlevel2example
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2example.databinding.ItemReminderBinding
+
+class ReminderAdapter(private val reminders: List<Reminder>) : RecyclerView.Adapter<ReminderAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_reminder, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return reminders.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.dataBind(reminders[position])
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val binding = ItemReminderBinding.bind(itemView)
+
+        fun dataBind(reminder: Reminder) {
+            binding.tvReminder.text = reminder.reminderText
+        }
+    }
+
+}
